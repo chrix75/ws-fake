1defcc7b2810d8a3ebf6108a2a0defb6ff1d1c42
Christian Sperandio <christian.sperandio@gmail.com>
2020-12-12 11:57:44 +0100

    correct fake failed test

--- src/test/java/com/example/azure/wsfake/WsFakeApplicationTests.java
+++ src/test/java/com/example/azure/wsfake/WsFakeApplicationTests.java
@@ -3,14 +3,11 @@ package com.example.azure.wsfake;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
 @SpringBootTest
 class WsFakeApplicationTests {
 
     @Test
     void contextLoads() {
-        fail("To stop deployment");
     }
 
 }
