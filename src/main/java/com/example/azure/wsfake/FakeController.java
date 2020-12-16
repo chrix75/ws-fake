7ecfaaad96a693e647328d8dfc09863e91f0c965
Christian Sperandio <christian.sperandio@gmail.com>
2020-12-16 22:54:28 +0100

    fetch secret value from key vault

--- src/main/java/com/example/azure/wsfake/FakeController.java
+++ src/main/java/com/example/azure/wsfake/FakeController.java
@@ -1,5 +1,9 @@
 package com.example.azure.wsfake;
 
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.security.keyvault.secrets.SecretClient;
+import com.azure.security.keyvault.secrets.SecretClientBuilder;
+import com.azure.security.keyvault.secrets.models.KeyVaultSecret;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,7 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class FakeController {
     @GetMapping("/value")
     public Result getResult() {
-        return new Result("Foo Bar");
+        SecretClient secretClient = new SecretClientBuilder()
+                .vaultUrl("https://kv-csp-test-secret.vault.azure.net")
+                .credential(new DefaultAzureCredentialBuilder().build())
+                .buildClient();
+
+        KeyVaultSecret test = secretClient.getSecret("test");
+        return new Result("Foo Bar " + test.getValue());
     }
 
     @PostMapping("/value/{value}")
